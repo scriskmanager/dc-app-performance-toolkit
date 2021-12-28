c98f594b8d5d7ba2d47c74bc7d25e10f459f5205
scriskmanager <76999220+scriskmanager@users.noreply.github.com>
2021-12-28 17:17:38 +0200

    Add files via upload

--- app/selenium_ui/jira_ui.py
+++ app/selenium_ui/jira_ui.py
@@ -63,6 +63,11 @@ Refer to `app/selenium_ui/jira/modules.py` for examples.
 # def test_1_selenium_custom_action(jira_webdriver, jira_datasets, jira_screen_shots):
 #     extension_ui.app_specific_action(jira_webdriver, jira_datasets)
 
+def test_1_selenium_view_project_matrix(jira_webdriver, jira_datasets, jira_screen_shots):
+    modules.view_project_matrix(jira_webdriver, jira_datasets)
+
+def test_1_selenium_view_project_rm_table(jira_webdriver, jira_datasets, jira_screen_shots):
+    modules.view_project_rm_table(jira_webdriver, jira_datasets)
 
 # this action should be the last one
 def test_2_selenium_z_log_out(jira_webdriver, jira_datasets, jira_screen_shots):
